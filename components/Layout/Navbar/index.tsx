26dc7c6575be4b7424ca37e063781b1cfebc72f7
LukmanRambe <lukmanrambe1945@gmail.com>
2023-09-27 12:58:05 +0700

    style: change navbar logo style

--- components/Layout/Navbar/index.tsx
+++ components/Layout/Navbar/index.tsx
@@ -5,7 +5,9 @@ type NavbarPropsType = {
 const Navbar: React.FC<NavbarPropsType> = ({ pageTitle }) => {
   return (
     <header className="fixed top-0 w-full px-5 py-4 bg-white border-b-[1px] -border--primary-20 text-end z-[1]">
-      <h1>{pageTitle}</h1>
+      <h1 className="text-xl font-bold tracking-wide -text--primary">
+        {pageTitle}
+      </h1>
     </header>
   );
 };
